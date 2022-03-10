86d9938e301d8ccba4467569a045c047e60010c7
Miguel Freire <m.b.freire@hotmail.com>
2022-03-10 12:35:19 +0000

    Updates site, enables form

---
+++ _next/static/E6Xp-AH7Lc-cp-MOoJvLI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/358-0be487bc78d083cb.js","static/chunks/pages/index-eabd90c8f99ac3aa.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
