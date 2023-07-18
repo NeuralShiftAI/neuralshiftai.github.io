8aa86e123c0144ed5314d53b169dda29283e141a
Miguel Freire <MiguelFreire@users.noreply.github.com>
2023-07-18 14:00:48 +0100

    fixes project number

--- _next/static/vGoA9kHc4IwcT_85-Jqef/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/880-7e637ff4731a5e9a.js","static/chunks/pages/index-d2dd21115ef53ea4.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
