fcb13791686b684698fe778b95caf9cd5fbe4b3c
Miguel Freire <m.b.freire@hotmail.com>
2023-10-23 12:18:13 +0100

    updates advisory, adds affine, changes mission

---
+++ _next/static/28b_JWjG6oYn8_4wNTbia/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/880-7e637ff4731a5e9a.js","static/chunks/pages/index-3a864fce3256ca9a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
